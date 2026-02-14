623d63b47d553d56d8b3cb8bf5309d3a3a94cfac
JoaoGuimaraes22 <jssgmrs22@gmail.com>
2026-02-14 01:09:38 +0000

    Small dict changes

--- app/components/HeroContent/HeroContent.tsx
+++ app/components/HeroContent/HeroContent.tsx
@@ -9,6 +9,9 @@ type HeroDict = {
   ctaPrimary: string;
   ctaSecondary: string;
   phone: string;
+  badge1: string;
+  badge2: string;
+  badge3: string;
 };
 
 type Props = {
@@ -98,10 +101,10 @@ export default function HeroContent({ dict, locale }: Props) {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
+              d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.746 3.746 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
             />
           </svg>
-          <span>Factory Certified</span>
+          <span>{dict.badge1}</span>
         </div>
         <div className="flex items-center gap-2">
           <svg
@@ -114,10 +117,10 @@ export default function HeroContent({ dict, locale }: Props) {
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
             />
           </svg>
-          <span>17+ Years Experience</span>
+          <span>{dict.badge2}</span>
         </div>
         <div className="flex items-center gap-2">
           <svg
@@ -127,7 +130,7 @@ export default function HeroContent({ dict, locale }: Props) {
           >
             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
           </svg>
-          <span>4.9 ★ Google Rating</span>
+          <span>{dict.badge3}</span>
         </div>
       </div>
     </div>
